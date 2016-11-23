6bb70cd66d7c423e93bfa57a77bba79987712ade
Su Di <sudi570@lu.com>
2016-11-23 16:32:18 +0800

    增强隐藏控件的算法，使用高度的约束

--- QuickVFL/src/demo/QVisibilityViewController.m
+++ QuickVFL/src/demo/QVisibilityViewController.m
@@ -11,6 +11,8 @@
 @interface QVisibilityViewController ()
 @property (nonatomic, weak) NSLayoutConstraint* constraintHideA;
 @property (nonatomic, weak) NSLayoutConstraint* constraintHideB;
+@property (nonatomic, weak) NSLayoutConstraint* constraintHideC;
+@property (nonatomic, weak) NSLayoutConstraint* constraintHideD;
 @end
 
 @implementation QVisibilityViewController
@@ -21,60 +23,106 @@
     
     UIView* viewAWrapper = QUICK_SUBVIEW(self.viewContentView, UIView);
     UILabel* labelA = QUICK_SUBVIEW(viewAWrapper, UILabel);
+    UILabel* labelA1 = QUICK_SUBVIEW(viewAWrapper, UILabel);
     
     UIView* viewBWrapper = QUICK_SUBVIEW(self.viewContentView, UIView);
     UILabel* labelB = QUICK_SUBVIEW(viewBWrapper, UILabel);
+    UILabel* labelB1 = QUICK_SUBVIEW(viewBWrapper, UILabel);
     
-    UIView* viewBarrierA = QUICK_SUBVIEW(self.viewContentView, UIView);
-    UIView* viewBarrierB = QUICK_SUBVIEW(self.viewContentView, UIView);
+    UILabel* labelC = QUICK_SUBVIEW(self.viewContentView, UILabel);
+    UILabel* labelD = QUICK_SUBVIEW(self.viewContentView, UILabel);
     
-    UIButton* buttonControlA = QUICK_SUBVIEW(self.viewContentView, UIButton);
-    UIButton* buttonControlB = QUICK_SUBVIEW(self.viewContentView, UIButton);
+    UIView* viewButtonWrapper = QUICK_SUBVIEW(self.viewContentView, UIView);
+    UIButton* buttonControlA = QUICK_SUBVIEW(viewButtonWrapper, UIButton);
+    UIButton* buttonControlB = QUICK_SUBVIEW(viewButtonWrapper, UIButton);
+    UIButton* buttonControlC = QUICK_SUBVIEW(viewButtonWrapper, UIButton);
+    UIButton* buttonControlD = QUICK_SUBVIEW(viewButtonWrapper, UIButton);
     
     NSString* layout = @"\
-    V:|[viewAWrapper]-(0@500)-[viewBarrierA(0)][viewBWrapper]-(0@500)-[viewBarrierB(0)] {left, right};\
-    V:[viewBarrierB]-20-[buttonControlA(44)]-| {left};\
-    V:[viewBarrierB]-20-[buttonControlB] {right};\
-    H:|-[buttonControlA]-[buttonControlB(buttonControlA)]-| {bottom};\
-        H:|[labelA]|; V:|[labelA]|;\
-        H:|[labelB]|; V:|[labelB]|;\
+    V:|[viewAWrapper][viewBWrapper][labelC][labelD][viewButtonWrapper]-| {left, right};\
+    H:|[viewAWrapper]|;\
+        H:|-[buttonControlA]-|;\
+        V:|-[buttonControlA]-[buttonControlB]-[buttonControlC]-[buttonControlD]-| {left, right};\
+        H:|-[buttonControlA]-|;\
+        H:|[labelA]|; V:|-(8@500)-[labelA]-(8@500)-[labelA1]-(8@500)-| {left, right};\
+        H:|[labelB]|; V:|-(8@500)-[labelB]-(8@500)-[labelB1]-(8@500)-| {left, right};\
     ";
     
     [self.viewContentView q_addConstraintsByText:layout
-    involvedViews:NSDictionaryOfVariableBindings(viewAWrapper, labelA, viewBarrierA, viewBWrapper, labelB, viewBarrierB, buttonControlA, buttonControlB)];
+    involvedViews:NSDictionaryOfVariableBindings(viewAWrapper, labelA, labelA1, viewBWrapper, labelB, labelB1, buttonControlA, buttonControlB, buttonControlC, buttonControlD, viewButtonWrapper, labelC, labelD)];
     
     // add extra control constraint
-    self.constraintHideA = [self.viewContentView q_addConstraintsByText:@"V:|-(0@250)-[viewBarrierA];" involvedViews:NSDictionaryOfVariableBindings(viewBarrierA)][0];
-    self.constraintHideB = [self.viewContentView q_addConstraintsByText:@"V:[viewBarrierA]-(0@250)-[viewBarrierB];" involvedViews:NSDictionaryOfVariableBindings(viewBarrierB, viewBarrierA)][0];
+    self.constraintHideA = [self.viewContentView q_addConstraintsByText:@"V:[viewAWrapper(0@250)];" involvedViews:NSDictionaryOfVariableBindings(viewAWrapper)][0];
+    self.constraintHideB = [self.viewContentView q_addConstraintsByText:@"V:[viewBWrapper(0@250)];" involvedViews:NSDictionaryOfVariableBindings(viewBWrapper)][0];
+    self.constraintHideC = [labelC q_addConstraintsByText:@"V:[labelC(0@250)];" involvedViews:NSDictionaryOfVariableBindings(labelC)][0];
+    self.constraintHideD = [labelD q_addConstraintsByText:@"V:[labelD(0@250)];" involvedViews:NSDictionaryOfVariableBindings(labelD)][0];
     
     
     labelA.text = @"A label.";
+    labelA1.text = @"A extend label";
     labelB.text = @"B label.";
+    labelB1.text = @"B extend label";
+    labelC.text = @"C label.";
+    labelD.text = @"D label.";
     labelA.backgroundColor = [UIColor greenColor];
+    labelA1.backgroundColor = [UIColor grayColor];
     labelB.backgroundColor = [UIColor blueColor];
+    labelB1.backgroundColor = [UIColor grayColor];
+    labelC.backgroundColor = [UIColor redColor];
+    labelD.backgroundColor = [UIColor yellowColor];
+    
+    viewAWrapper.clipsToBounds = YES;
+    viewBWrapper.clipsToBounds = YES;
+    
+    viewAWrapper.backgroundColor = [UIColor lightGrayColor];
+    viewBWrapper.backgroundColor = [UIColor darkGrayColor];
     
     buttonControlA.backgroundColor = [UIColor orangeColor];
     [buttonControlA setTitle:@"Contron A" forState:UIControlStateNormal];
     [buttonControlA q_addTapAction:^(UIButton *sender) {
-        if(self.constraintHideA.priority >= 750){
-            self.constraintHideA.priority = 250;
-            viewAWrapper.hidden = NO;
+        if(self.constraintHideA.priority >= UILayoutPriorityDefaultHigh){
+            self.constraintHideA.priority = UILayoutPriorityDefaultLow;
         }else{
-            self.constraintHideA.priority = 750;
-            viewAWrapper.hidden = YES;
+            self.constraintHideA.priority = UILayoutPriorityDefaultHigh + 1;
         }
+        
+        [self refreshContent];
     }];
     
     buttonControlB.backgroundColor = [UIColor orangeColor];
     [buttonControlB setTitle:@"Contron B" forState:UIControlStateNormal];
     [buttonControlB q_addTapAction:^(UIButton *sender) {
-        if(self.constraintHideB.priority >= 750){
-            self.constraintHideB.priority = 250;
-            viewBWrapper.hidden = NO;
+        if(self.constraintHideB.priority >= UILayoutPriorityDefaultHigh){
+            self.constraintHideB.priority = UILayoutPriorityDefaultLow;
         }else{
-            self.constraintHideB.priority = 750;
-            viewBWrapper.hidden = YES;
+            self.constraintHideB.priority = UILayoutPriorityDefaultHigh + 1;
         }
+        
+        [self refreshContent];
+    }];
+    
+    buttonControlC.backgroundColor = [UIColor orangeColor];
+    [buttonControlC setTitle:@"Contron C" forState:UIControlStateNormal];
+    [buttonControlC q_addTapAction:^(UIButton *sender) {
+        if(self.constraintHideC.priority >= UILayoutPriorityDefaultHigh){
+            self.constraintHideC.priority = UILayoutPriorityDefaultLow;
+        }else{
+            self.constraintHideC.priority = UILayoutPriorityDefaultHigh + 1;
+        }
+        
+        [self refreshContent];
+    }];
+    
+    buttonControlD.backgroundColor = [UIColor orangeColor];
+    [buttonControlD setTitle:@"Contron D" forState:UIControlStateNormal];
+    [buttonControlD q_addTapAction:^(UIButton *sender) {
+        if(self.constraintHideD.priority >= UILayoutPriorityDefaultHigh){
+            self.constraintHideD.priority = UILayoutPriorityDefaultLow;
+        }else{
+            self.constraintHideD.priority = UILayoutPriorityDefaultHigh + 1;
+        }
+        
+        [self refreshContent];
     }];
     
     [self refreshContent];
